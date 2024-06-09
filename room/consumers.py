e6988301eb58a090484b63ef1c2ae7965d6e709f
dwijgohil <20ecuot047@ddu.ac.in>
2024-06-09 16:22:54 +0530

    message storing functionality done.

--- room/consumers.py
+++ room/consumers.py
@@ -4,6 +4,9 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 from asgiref.sync import sync_to_async
 from twisted.internet.defer import ensureDeferred
 
+from django.contrib.auth.models import User
+from . models import Room,Message
+
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
@@ -27,7 +30,9 @@ class ChatConsumer(AsyncWebsocketConsumer):
         message = data['message']
         username = data['username']
         room = data['room']
-
+        
+        await self.save_message(username, room, message)
+        
         await self.channel_layer.group_send(
             self.room_group_name,
             {
@@ -48,6 +53,13 @@ class ChatConsumer(AsyncWebsocketConsumer):
             'username': username,    
             'room': room,        
         }))
+
+    @sync_to_async
+    def save_message(self, username, room, message):
+        user = User.objects.get(username=username)
+        room = Room.objects.get(slug=room)
+
+        Message.objects.create(user=user, room=room, content=message)
 
         
 
--- room/views.py
+++ room/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
-from .models import Room
+from .models import Room,Message
 # Create your views here.
 
 @login_required
@@ -12,5 +12,6 @@ def rooms(request):
 @login_required
 def room(request, slug):
     room = Room.objects.get(slug=slug)
-    return render(request, 'room/room.html', {'room': room})
+    messages = Message.objects.filter(room=room)[0:25]
+    return render(request, 'room/room.html', {'room': room, 'messages': messages})
 
